3439725865e2036001868fe8ad22eb9df149b33b
Rohit Kumar <rohit9075@gmail.com>
2018-10-15 13:06:52 +0530

    photo list landscape

--- app/src/main/java/com/example/flickerapp/activities/MainActivity.java
+++ app/src/main/java/com/example/flickerapp/activities/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.flickerapp.activities;
 
+import android.content.res.Configuration;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.flickerapp.R;
 import com.example.flickerapp.fragments.ImageListFragment;
+import com.example.flickerapp.fragments.PhotoDetailFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportFragmentManager().beginTransaction().add(R.id.container,new ImageListFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new ImageListFragment()).commit();
+
+//        getSupportFragmentManager().beginTransaction().replace(R.id.containerforPhotoDetail,new PhotoDetailFragment()).commit();
+
+
     }
+
 }
