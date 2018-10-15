42c20c0329f27de57327ef9048a8c2d09f4b4c31
Rohit Kumar <rohit9075@gmail.com>
2018-10-15 21:47:52 +0530

    Handling screen orientation dynamically

--- app/src/main/java/com/example/flickerapp/activities/MainActivity.java
+++ app/src/main/java/com/example/flickerapp/activities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.flickerapp.activities;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Surface;
+import android.view.WindowManager;
 
 import com.example.flickerapp.R;
 import com.example.flickerapp.fragments.ImageListFragment;
@@ -16,9 +20,33 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.container,new ImageListFragment()).commit();
 
+        getScreenOrientation();
 
     }
+
+
+
+    public void getScreenOrientation()
+    {
+        Display screenOrientation = getWindowManager().getDefaultDisplay();
+        int orientation = Configuration.ORIENTATION_UNDEFINED;
+
+        if(screenOrientation.getWidth() < screenOrientation.getHeight()){
+            // orientation = Configuration.ORIENTATION_PORTRAIT;
+
+            // portrait
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,new ImageListFragment()).commit();
+
+        }else {
+            //  orientation = Configuration.ORIENTATION_LANDSCAPE;
+            // Landscape
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,new ImageListFragment()).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.containerforPhotoDetail,new PhotoDetailFragment()).commit();
+            //Do something
+
+        }
+    }
+
 
 }
